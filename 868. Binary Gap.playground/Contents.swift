c59d17676ae5a513b84a4a1b3c4372bbdd7c6bf4
Bernard <hurgoon@gmail.com>
2019-02-11 02:34:21 +0900

    a = b.enumerated().compactMap { $0.element == "1" ? $0.offset : nil }

--- 868. Binary Gap.playground/Contents.swift
+++ 868. Binary Gap.playground/Contents.swift
@@ -41,13 +41,58 @@
  Note:
  
  1 <= N <= 10^9
-*/
+ */
 
 import UIKit
 
 class Solution {
     func binaryGap(_ N: Int) -> Int {
         
-        return 0
+        let binaryArray = Array(String(N, radix: 2))
+        var digitArray: Array<Int> = []
+        
+        for i in 0..<binaryArray.count {
+            if binaryArray[i] == "1" {
+                digitArray.append(i)
+            }
+        }
+        
+        var distance = 0
+        var maxDistance = 0
+        
+        for i in 0..<digitArray.count-1 {
+            distance = digitArray[i+1] - digitArray[i]
+            
+            if maxDistance < distance {
+                maxDistance = distance
+            }
+        }
+        return maxDistance
     }
 }
+
+let a = Solution()
+a.binaryGap(50000)
+
+class Solution2 {
+    func binaryGap(_ N: Int) -> Int {
+        
+        let binaryArray = Array(String(N, radix: 2))
+        let digitArray = binaryArray.enumerated().compactMap {
+            $0.element == "1" ? $0.offset : nil
+        }
+        
+        var distance = 0
+        var maxDistance = 0
+        
+        for i in 0..<digitArray.count-1 {
+            distance = digitArray[i+1] - digitArray[i]
+            
+            maxDistance = maxDistance < distance ? distance : maxDistance
+        }
+        return maxDistance
+    }
+}
+
+let b = Solution2()
+b.binaryGap(50000)
